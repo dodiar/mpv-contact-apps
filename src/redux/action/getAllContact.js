0a8dbabec3d7e6c43a060ad9687c96175385e077
USER\asus1 <dodi.arahmat@gmail.com>
2025-03-10 13:49:22 +0700

    create action integration all data on views

---
+++ src/redux/action/getAllContact.js
@@ -0,0 +1,28 @@
+import { type } from "@testing-library/user-event/dist/type";
+import axios from "axios";
+import Swal from "sweetalert2";
+
+const allContact = () => {
+    return function (dispatch) {
+        dispatch({type: "LOADING", payload:true});
+        axios({
+            url: "http://localhost:3001/contacts",
+            method: "get"
+        })
+        .then(({data}) => {
+            dispatch({type: "LOADING", payload:false});
+            dispatch({type: "ALL_CONTACT", payload:data})
+        }).catch((err) => {
+            dispatch({type: "LOADING", payload:false});
+            Swal.fire({
+                position: "top-end",
+                icon: "error",
+                title: `${err.message} ${err.status}`,
+                showCloseButton: false,
+                timer: 1000
+            })
+        });
+    }
+}
+
+export default allContact;
--- src/views/ContactApps.jsx
+++ src/views/ContactApps.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+// action
+import getAllContact from '../redux/action/getAllContact';
 
 export default function ContactApps() {
+    const dispatch = useDispatch();
+    const loading = useSelector(state=>state.loadingReducer.loading);
+    const contacts = useSelector(state=>state.contactReducer.allContact);
+
+    useEffect(() => {
+        dispatch(getAllContact())
+    }, []);
     return (
-        <p>Homepage</p>
+        <>
+            <div className="container mt-2">
+                <div className="row">
+                    {
+                        loading ?
+                        <div className="d-flex justify content center mt-5">
+                            <div className="spinner-border" role="status">
+                                <span className="visually-hidden">Loading ...</span>
+                            </div>
+                        </div>
+                        :
+                        contacts.lenght === 0 ?
+                        <div className="text-center m-2 p-4">Data Not Found</div>
+                        :
+                        contacts.map(contact=>{
+                            return (
+                                <div className="col-md-3 p-1">
+                                    <div className="card shadow-sm radius">
+                                        <div className="card-body">
+                                            <h5>{contact.name}</h5>
+                                            <p>{contact.email}</p>
+                                            <p>{contact.phonenumber}</p>
+                                            <p>Category: {contact.category}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </>
     );
 }
