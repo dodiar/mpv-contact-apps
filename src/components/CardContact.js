3a45a97c1a4992df3e7e8a2633fcafcfbc4aebb6
USER\asus1 <dodi.arahmat@gmail.com>
2025-03-13 16:05:02 +0700

    create delete api integration and view new update data

--- src/components/CardContact.js
+++ src/components/CardContact.js
@@ -1,8 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import UpdateContact from "./UpdateContactForm";
+import { useDispatch } from "react-redux";
+import Swal from "sweetalert2";
+import deleteContact from "../redux/action/deleteContact";
 
 export default function CardContact(props) {
+    const dispatch = useDispatch();
+
+    const DeleteContact = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You want to delete this contact!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085D6",
+            cancelButtonColor: "#D39",
+            confirmButtonText: "Yes, delete it!"
+        })
+        .then((result) => {
+            if (result.isConfirmed) {
+                dispatch(deleteContact(id))
+            }    
+        });
+    }
+
     return (
         <div className="card shadow-sm radius">
             <div className="card-body">
@@ -20,6 +42,7 @@ export default function CardContact(props) {
                         <button className="btn btn-dark">detail</button>
                     </Link>
                     <UpdateContact idContact={props.contact.id}/>
+                    <button onClick={() => DeleteContact(props.contact.id)} className="btn btn-dark">Delete</button>
                 </div>
             }
         </div>
